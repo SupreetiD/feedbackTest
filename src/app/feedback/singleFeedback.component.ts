af8105198c0e9b6855d231aa99b7c0beb7f3c5cf
Supreeti Dutta <supreeti@fundshop.fr>
2019-04-25 00:30:37 +0200

    Page 2 modifications

--- src/app/feedback/singleFeedback.component.ts
+++ src/app/feedback/singleFeedback.component.ts
@@ -7,6 +7,9 @@ import {FeedbackService} from '../services/feedbackService';
 })
 export class SingleFeedbackComponent implements OnInit {
   singleFeed: any;
+  reviews: any;
+  localeModel: any;
+  selectedReview: any ;
 
   constructor(public feedbackService: FeedbackService,
 
@@ -14,5 +17,19 @@ export class SingleFeedbackComponent implements OnInit {
 
   ngOnInit() {
     this.singleFeed = this.feedbackService.storage;
+    this.reviews = this.feedbackService.storage.translations;
+    this.selectedReview = this.feedbackService.storage.translations[0].content;
+    this.localeModel = this.feedbackService.storage.translations[0].locale;
+  }
+
+  updateSelectedValue(event: any) {
+    const me: any = this;
+    console.log('xftcygvui: ', event);
+    (me.reviews).forEach(function(review: any) {
+      if (review.locale === event ) {
+        me.selectedReview = review.content;
+        console.log('review: ', me.selectedReview);
+      }
+    });
   }
 }
